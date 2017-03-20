553352a1b720da3b1159af2bd6acc6774d12b37d
IxsCN <Ixs.CN@hotmail.com>
2017-03-20 23:12:41 +0800

    稳定性提高
    升级frpc到0.9.3，优化frpc参数。
    frp升级为最新版本，添加内网穿透状态显示，添加内网穿透守护进程，添加存储设备显示。
    添加存储设备容量显示。
    修复app无法显示状态的问题。

--- zip.sh
+++ zip.sh
@@ -7,7 +7,7 @@ mac=$(lua -e "print(require'tw'.get_mac())")
 mac=${mac:6:8}
 sed -i "s#$mac#FFFFFFFF#g" /etc/aria2gee/frp.ini
 
-tar -czvf /tmp/data/aria2gee/aria2gee.tar.gz /tmp/cryptdata/usr/bin/base64 /tmp/cryptdata/usr/bin/aria2c /tmp/cryptdata/usr/bin/frpc /etc/aria2gee/aria2c.conf /etc/aria2gee/aria2c.conf.demo /etc/aria2gee/frp.ini /etc/init.d/aria2gee.sh
+tar -czvf /tmp/data/aria2gee/aria2gee.tar.gz /tmp/cryptdata/usr/bin/base64 /tmp/cryptdata/usr/bin/aria2gee.monitor /tmp/cryptdata/usr/bin/aria2c /tmp/cryptdata/usr/bin/frpc /etc/aria2gee/aria2c.conf /etc/aria2gee/aria2c.conf.demo /etc/aria2gee/frp.ini /etc/init.d/aria2gee.sh
 
 tar -czvf /tmp/data/aria2gee/archive.tgz -C /tmp/data/aria2gee/ manifest.json script aria2gee.tar.gz Aria2Gee.conf
 echo -e "[\e[32m还原配置文件\e[37m]"
